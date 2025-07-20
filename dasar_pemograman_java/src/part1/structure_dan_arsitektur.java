399aa963b7480a5707332ed47a7dacdd475b8f87
esbatusadadda <juliakomorowicz16@gmail.com>
2025-07-20 01:56:25 -0400

    update

--- dasar_pemograman_java/src/part1/structure_dan_arsitektur.java
+++ dasar_pemograman_java/src/part1/structure_dan_arsitektur.java
@@ -1,15 +1,8 @@
-package pertemuan1; // 1 deklarasi Package
+package part1; // 1 deklarasi Package
 
 import java.util.Scanner; // 2 bagian import library
-/**
- *
- * @author RUSNA
- */
+
 public class structure_dan_arsitektur { // bagian kelas utama
-    
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Masukkan Nama");
