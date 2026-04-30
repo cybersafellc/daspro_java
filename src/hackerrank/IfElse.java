c28333a61737fd0142bb0f0ab3486b74376da21d
myNameIsNewSpirit <rusnandapurnama@gmail.com>
2026-04-30 14:48:44 +0700

    delete trash package/class

--- src/hackerrank/IfElse.java
+++ src/hackerrank/IfElse.java
@@ -9,8 +9,6 @@ import java.util.concurrent.*;
 import java.util.function.*;
 import java.util.regex.*;
 import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 
 
