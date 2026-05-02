b045aab1f015a76c3f40d50d3771a8f9763c92d7
myNameIsNewSpirit <rusnandapurnama@gmail.com>
2026-05-02 11:04:28 +0700

    hackerrank - string reverse & string compater tricky (use equals method)

--- src/hackerrank/JavaStringReverse.java
+++ src/hackerrank/JavaStringReverse.java
@@ -1,4 +1,20 @@
 package hackerrank;
 
+import java.util.*;
+
 public class JavaStringReverse {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        scan.hasNext();
+        String input = scan.nextLine();
+        String reverseInput = "";
+        for(int i = 0; i < input.length(); i++){
+            reverseInput = input.charAt(i) + reverseInput;
+        }
+        if(reverseInput.equals(input)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
 }
