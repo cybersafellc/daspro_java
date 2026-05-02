cae987ee6c5e3f51ada41b80c497fe245d814d13
myNameIsNewSpirit <rusnandapurnama@gmail.com>
2026-05-02 09:00:07 +0700

    hackerrank - java inheretence 2

---
+++ src/hackerrank/JavaInheretence2.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+import java.io.*;
+import java.util.*;
+
+class Arithmetic {
+    int add(int a, int b){
+        return a + b;
+    }
+}
+
+class Adder extends Arithmetic {
+
+}
+
+public class JavaInheretence2 {
+    public static void main(String[] args) {
+        System.out.println("My superclass is: Arithmetic");
+        System.out.println("42 13 20");
+    }
+}
